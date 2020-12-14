1f68021cd82781cd8e7fc8c57d4cd74cb1c8511b
xyy <1040777689@qq.com>
2020-12-14 09:42:35 +0800

    子任务4-2

--- README.md
+++ README.md
@@ -11,3 +11,14 @@
 ​	模型为	haarcascade_frontalface_default.xml
 
 直接运行即可复现结果，重复运行会报错（重复创建文件）。
+
+
+子任务4： 人脸识别
+
+运行环境：
+
+python 3.6
+
+face_recognition
+
+将图片文件夹放在根目录下即可复现，pred_test中少数图片未能识别出来。
--- faceIdentification2.py
+++ faceIdentification2.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 # 路径参数配置
 basefacefilespath = "face_identification\gallery"  # faces文件夹中放待识别任务正面图,文件名为人名,将显示于结果中
-destfacefilepath = "face_identification\probe_val"  # 用于识别的目标图片目录
+destfacefilepath = "face_identification\probe_test"  # 用于识别的目标图片目录
 
 
 def cos_sim(vector_a, vector_b):
@@ -42,13 +42,12 @@ def paint_chinese_opencv(im, chinese, pos, color):
 # 余弦相似度识别阈值
 tolerance = 0.96
 
-
 # 加载待识别人脸图像并识别。
 baseface_titles = []  # 图片名字列表
 baseface_face_encodings = []  # 识别所需人脸编码结构集
 # 读取人脸资源
 for fn in os.listdir(basefacefilespath):  # fn 人脸文件名
-    if(len(face_recognition.face_encodings(face_recognition.load_image_file(basefacefilespath + "/" + fn)))>0):
+    if (len(face_recognition.face_encodings(face_recognition.load_image_file(basefacefilespath + "/" + fn))) > 0):
         baseface_face_encodings.append(
             face_recognition.face_encodings(face_recognition.load_image_file(basefacefilespath + "/" + fn))[0])
         # fn = fn.split("_")[1]
@@ -56,6 +55,9 @@ for fn in os.listdir(basefacefilespath):  # fn 人脸文件名
         baseface_titles.append(fn)
         # print(fn)
 print("================begin================")
+
+res = open("pred_test.txt", "w")
+
 # 从识别库中读取一张图片并识别
 for fd in os.listdir(destfacefilepath):
     # 获取一张图片
@@ -65,7 +67,7 @@ for fd in os.listdir(destfacefilepath):
     # 人脸检测,并获取帧中所有人脸编码
     # 参数说明，number_of_times_to_upsample可以理解为迭代次数，次数越大，人脸检测越准确，但是运行耗时越长。
     # model默认为hog，运行速度快，但是识别率不高。后面第2张图分辨率低，所以必须用cnn并且迭代次数为2
-    face_locations = face_recognition.face_locations(faceData, number_of_times_to_upsample=2, model="cnn")
+    face_locations = face_recognition.face_locations(faceData, number_of_times_to_upsample=2, model="hog")
     face_encodings = face_recognition.face_encodings(faceData, face_locations)
     # 遍历图片中所有人脸编码
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
@@ -78,23 +80,24 @@ for fd in os.listdir(destfacefilepath):
             if match >= tolerance:
                 name = baseface_titles[i]
                 print("识别出：" + name)
+                res.writelines(fd + " " + name + "\n")
                 break
 
         # 围绕脸的框
-        cv2.rectangle(faceData, (left, top), (right, bottom), (0, 0, 255), 2)
+        # cv2.rectangle(faceData, (left, top), (right, bottom), (0, 0, 255), 2)
 
         # 如果遇到没有识别出的人脸，则跳过
         if name == "?":
             continue
 
         # 框下的名字(即,匹配的图片文件名)
-        cv2.rectangle(faceData, (left, bottom), (right, bottom + 25), (0, 0, 255), cv2.FILLED)
+        # cv2.rectangle(faceData, (left, bottom), (right, bottom + 25), (0, 0, 255), cv2.FILLED)
         # faceData = cv2.putText(faceData, name,(left + 2, bottom + 12), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255, 255, 255),1)
-        faceData = paint_chinese_opencv(faceData, name, (left + 2, bottom + 4), (255, 255, 255))
+        # faceData = paint_chinese_opencv(faceData, name, (left + 2, bottom + 4), (255, 255, 255))
     # frame = ft.draw_text(frame, (left + 2, bottom + 12), name, 16,  (255, 255, 255))
 
     # show结果图像
-    cv2.imshow(fd, cv2.cvtColor(faceData, cv2.COLOR_BGR2RGB))
-
-cv2.waitKey()
-cv2.destroyAllWindows()
+    # cv2.imshow(fd, cv2.cvtColor(faceData, cv2.COLOR_BGR2RGB))
+res.close()
+# cv2.waitKey()
+# cv2.destroyAllWindows()
